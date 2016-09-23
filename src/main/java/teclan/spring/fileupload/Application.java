d1c4f1d78f2e4bf67147f5fd4b6af4d321e638f4
tanbingjian <tanbingjian@maoguang.org>
2016-09-23 16:00:48 +0800

    文件批量上传

---
+++ src/main/java/teclan/spring/fileupload/Application.java
@@ -0,0 +1,25 @@
+package teclan.spring.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    // @Bean
+    // public MultipartConfigElement multipartConfigElement() {
+    // MultipartConfigFactory factory = new MultipartConfigFactory();
+    // factory.setMaxFileSize("128KB");
+    // factory.setMaxRequestSize("128KB");
+    // return factory.createMultipartConfig();
+    // }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
