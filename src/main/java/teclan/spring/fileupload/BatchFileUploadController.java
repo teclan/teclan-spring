c6d6898e00a980532201d773c41ff9ffef118c8a
tanbingjian <tanbingjian@maoguang.org>
2016-09-23 16:40:35 +0800

    文件下载

--- src/main/java/teclan/spring/fileupload/BatchFileUploadController.java
+++ src/main/java/teclan/spring/fileupload/BatchFileUploadController.java
@@ -3,10 +3,18 @@ package teclan.spring.fileupload;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +24,9 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller
 public class BatchFileUploadController {
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(BatchFileUploadController.class);
+
     @RequestMapping(value = "/batch/upload", method = RequestMethod.POST)
     public @ResponseBody String handleFileUpload(HttpServletRequest request) {
         List<MultipartFile> files = ((MultipartHttpServletRequest) request)
@@ -47,4 +58,23 @@ public class BatchFileUploadController {
         }
         return "upload successful";
     }
+
+    @RequestMapping("download")
+    public ResponseEntity<byte[]> download() throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+
+        String path = "/home/dev/1xxx.odt";
+        File file = new File(path);
+
+        if (!file.exists()) {
+            LOGGER.error("File is not exist : {}", file.getAbsolutePath());
+            return null;
+        }
+
+        headers.setContentDispositionFormData("attachment", file.getName());
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
+                headers, HttpStatus.CREATED);
+    }
+
 }
